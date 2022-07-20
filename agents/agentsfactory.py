a880be28d4e1db104d3db436380673fb6ce0ad0e
yonicohen96 <61869789+yonicohen96@users.noreply.github.com>
2022-07-20 20:29:23 +0300

    added dynamic agents to factory

--- agents/agentsfactory.py
+++ agents/agentsfactory.py
@@ -2,6 +2,8 @@ from agents.agent import AgentInfo
 from agents.dynamicagent import DynamicAgent
 from agents.staticagent import StaticAgent
 from agents.dynamicagents.dynamicrlagent import DynamicRLAgent
+from agents.dynamicagents.humanagent import HumanAgent
+from agents.dynamicagents.MinExpensesAgent import MinExpensesAgent
 from agents.staticagents.staticrlagent import StaticRLAgent
 from agents.staticagents.brutefroceagent import BruteForceAgent
 from agents.staticagents.geneticalgorithmagent import GeneticAlgorithmAgent
@@ -11,35 +13,43 @@ from typing import List
 
 class AgentsFactory:
     AGENT_DYNAMIC_RL = "dynamic_RL"
+    AGENT_DYNAMIC_HUMAN = "human"
+    AGENT_DYNAMIC_MIN_EXPENSES = "min_expenses"
     AGENT_STATIC_RL = "static_RL"
-    AGENT_BRUTEFORCE = "brute_force"
-    AGENT_GENETIC_ALGORITHM = "genetic_algorithm"
-    AGENT_SIMULATED_ANNEALING = "simulated_annealing"
+    AGENT_STATIC_BRUTEFORCE = "brute_force"
+    AGENT_STATIC_GENETIC_ALGORITHM = "genetic_algorithm"
+    AGENT_STATIC_SIMULATED_ANNEALING = "simulated_annealing"
 
     @staticmethod
     def build_dynamic_agent(choice: str, agent_info: AgentInfo) -> DynamicAgent:
         if choice == AgentsFactory.AGENT_DYNAMIC_RL:
             return DynamicRLAgent(agent_info)
+        elif choice == AgentsFactory.AGENT_DYNAMIC_HUMAN:
+            return HumanAgent(agent_info)
+        if choice == AgentsFactory.AGENT_DYNAMIC_MIN_EXPENSES:
+            return MinExpensesAgent(agent_info)
         raise ValueError("no such agent")
 
     @staticmethod
     def build_static_agent(choice: str, agent_info: AgentInfo) -> StaticAgent:
         if choice == AgentsFactory.AGENT_STATIC_RL:
             return StaticRLAgent(agent_info)
-        elif choice == AgentsFactory.AGENT_BRUTEFORCE:
+        elif choice == AgentsFactory.AGENT_STATIC_BRUTEFORCE:
             return BruteForceAgent(agent_info)
-        elif choice == AgentsFactory.AGENT_GENETIC_ALGORITHM:
+        elif choice == AgentsFactory.AGENT_STATIC_GENETIC_ALGORITHM:
             return GeneticAlgorithmAgent(agent_info)
-        elif choice == AgentsFactory.AGENT_SIMULATED_ANNEALING:
+        elif choice == AgentsFactory.AGENT_STATIC_SIMULATED_ANNEALING:
             return SimulatedAnnealingAgent(agent_info)
         raise ValueError("no such agent")
 
     @staticmethod
     def get_static_agent_legal_values() -> List[str]:
-        return [AgentsFactory.AGENT_STATIC_RL, AgentsFactory.AGENT_BRUTEFORCE,
-                AgentsFactory.AGENT_GENETIC_ALGORITHM,
-                AgentsFactory.AGENT_SIMULATED_ANNEALING]
+        return [AgentsFactory.AGENT_STATIC_RL, AgentsFactory.AGENT_STATIC_BRUTEFORCE,
+                AgentsFactory.AGENT_STATIC_GENETIC_ALGORITHM,
+                AgentsFactory.AGENT_STATIC_SIMULATED_ANNEALING]
 
     @staticmethod
     def get_dynamic_agent_legal_values() -> List[str]:
-        return [AgentsFactory.AGENT_DYNAMIC_RL]
+        return [AgentsFactory.AGENT_DYNAMIC_RL,
+                AgentsFactory.AGENT_DYNAMIC_HUMAN,
+                AgentsFactory.AGENT_DYNAMIC_MIN_EXPENSES]
