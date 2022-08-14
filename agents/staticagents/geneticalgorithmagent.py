e4d17a06571e2f00c8c5fe0832f946b77cad8b9d
roikohno <roikohno@gmail.com>
2022-08-14 20:26:10 +0300

    genetic worksssssssss

--- agents/staticagents/geneticalgorithmagent.py
+++ agents/staticagents/geneticalgorithmagent.py
@@ -7,6 +7,7 @@ from data.trafficdatatypes import *
 from data.trafficgenerator import TrafficGenerator
 
 from typing import Tuple
+from scipy.special import softmax
 
 import itertools as it
 
@@ -14,11 +15,11 @@ import itertools as it
 class GeneticAlgorithmAgent(StaticAgent):
     BASE_NUMBER = 2
     INITIAL_POPULATION_SIZE: int = 200
-    SIMULATION_DAYS_NUM = 10
-    SELECTION_PERCENTILE = 80
-    DECAY_FACTOR = 0.9
+    SIMULATION_DAYS_NUM = 4
+    SELECTION_PERCENTILE = 95
+    DECAY_FACTOR = 0.8
     MUTATION_FACTOR = 0.8
-    NOISE_FACTOR = 0.5
+    NOISE_FACTOR = 0.25
 
     def __init__(self, agent_info: AgentInfo):
         super(GeneticAlgorithmAgent, self).__init__(agent_info)
@@ -28,17 +29,20 @@ class GeneticAlgorithmAgent(StaticAgent):
             GeneticAlgorithmAgent.INITIAL_POPULATION_SIZE)
 
     def spread_scooters(self) -> Tuple[List[NestAllocation], float]:
+        generation = 0
         while self._pop.shape[0] > 1:
+            generation += 1
+            print(f"Simulating generation: {generation}, population: {len(self._pop)}")
             fitness_vals: List[float] = []
             for ind in self._pop:
-                print(ind)
                 fitness_vals.append(self._simulate_individual(ind))
-            print(f'avg fitness {np.average(fitness_vals)}')
+            print(f'Avg {round(np.average(fitness_vals), 2)} '
+                  f'Max: {round(np.max(fitness_vals), 2)}\n'
+                  f'Argmax: {self._pop[np.argmax(fitness_vals)]}')
             parents: np.ndarray = self._fit(fitness_vals)
             offspring: np.ndarray = self._crossover(parents)
-            self._pop = self._mutate(offspring)
-            self._pop = self._pop.astype(np.int64)
-            print(self._pop.shape)
+            offspring = self._mutate(offspring)
+            self._pop = offspring
         # the population size is 1
         the_chosen_one = self._pop[0]
         return self._get_nests_spread(the_chosen_one), self._simulate_individual(the_chosen_one)
@@ -48,17 +52,15 @@ class GeneticAlgorithmAgent(StaticAgent):
                                                        GeneticAlgorithmAgent.SELECTION_PERCENTILE)]
 
     def _crossover(self, parents):
-        offspring = np.empty((0, len(self.agent_info.optional_nests)))
+        offspring = list()
         offspring_size = int(np.max([np.floor(
             self._pop.shape[0] * GeneticAlgorithmAgent.DECAY_FACTOR), 1]))
 
         # note that a parent can survive a generation and continue to the next one as if he
         # reproduces with itself, he is born anew
-        while offspring.shape[0] < offspring_size:
+        for i in range(offspring_size):
             parent1, parent2 = parents[np.random.choice(parents.shape[0], size=2), :]
-            new_offspring = self._reproduce(parent1, parent2)
-            offspring = np.vstack([offspring, new_offspring])
-            np.unique(offspring, axis=0)
+            offspring.append(self._reproduce(parent1, parent2))
         return np.array(offspring)
 
     def _mutate(self, offspring: np.ndarray) -> np.ndarray:
@@ -69,24 +71,17 @@ class GeneticAlgorithmAgent(StaticAgent):
                                    size=int(np.floor(GeneticAlgorithmAgent.MUTATION_FACTOR *
                                                      offspring.shape[0])),
                                    replace=False)
-        offspring[indices] = np.apply_along_axis(self._noise, 1, offspring[indices])
+        offspring[indices] = self._noise(offspring[indices])
         return offspring
 
     def _generate_population(self, pop_size: int) -> np.ndarray:
-        possible_scooters_spread = np.array([comb for comb in
-                                             it.product(
-                                                 range(self.agent_info.scooters_num + 1),
-                                                 repeat=len(self.agent_info.optional_nests))
-                                             if sum(comb) == self.agent_info.scooters_num])
-        if possible_scooters_spread.shape[0] <= pop_size:
-            return possible_scooters_spread
-        return possible_scooters_spread[np.random.choice(possible_scooters_spread.shape[0],
-                                                         size=pop_size, replace=False), :]
+        return softmax(np.random.random((pop_size, len(self.agent_info.optional_nests))), axis=-1)
+
 
     def _get_nests_spread(self, ind: np.ndarray) -> List[NestAllocation]:
-        # we assume that scooters_locations is an action and not a Map type
+        scooter_spread = self.discretize(ind)
         return [NestAllocation(self.agent_info.optional_nests[i], scooters_num)
-                for i, scooters_num in enumerate(ind)]
+                for i, scooters_num in enumerate(scooter_spread)]
 
     def _simulate_individual(self, ind) -> Union[np.ndarray, float]:
         total_revenue: List[float] = []
@@ -100,33 +95,46 @@ class GeneticAlgorithmAgent(StaticAgent):
                 traffic_simulator.get_simulation_result(ind_nests_locations)
             rides_completed: List[Ride] = result[0]
             next_day_locations: Map = result[1]
-
+            unused_scooters = (self.agent_info.scooters_num - len(rides_completed))
             # compute revenue
-            cur_revenue: float = self.agent_info.incomes_expenses.calculate_revenue(
+            cur_revenue = self.agent_info.incomes_expenses.calculate_revenue(
                 rides_completed, prev_scooters_locations, ind_nests_locations)
-            total_revenue.append(cur_revenue)
-
+            total_revenue.append(cur_revenue[0] - cur_revenue[1] - 5 * unused_scooters)
             prev_scooters_locations = next_day_locations
         return np.mean(total_revenue)
 
     def _reproduce(self, parent1, parent2) -> np.ndarray:
-        offspring = np.zeros(len(self.agent_info.optional_nests))
-        matched_parents = np.vstack([parent1, parent2])
-        while np.sum(offspring) < self.agent_info.scooters_num:
-            parent_idx = np.random.choice(matched_parents.shape[0])
-            entry_idx = np.random.choice(np.where(matched_parents[parent_idx] > 0)[0])
-            offspring[entry_idx] += 1
-            matched_parents[parent_idx][entry_idx] -= 1
+        # todo attempt 1: half of coordinates from each parent
+        # take_from_p1 = np.random.choice(np.arange(len(parent1)), len(parent1) // 2, replace=False)
+        take_from_p1 = np.random.random(len(parent1)) > 0.5
+        offspring = take_from_p1 * parent1 + (1 - take_from_p1) * parent2
+        offspring = offspring / offspring.sum()
         return offspring
+        # todo attempt 2: average of vectors then normalize
 
     def _noise(self, offspring: np.ndarray) -> np.ndarray:
-        scooters_to_displace = round(GeneticAlgorithmAgent.NOISE_FACTOR *
-                                     self.agent_info.scooters_num)
-        for _ in range(scooters_to_displace):
-            valid_indices = np.where(offspring > 0)[0]
-            replace = valid_indices.size == 1
-            from_entry_idx, to_entry_idx = np.random.choice(np.where(offspring > 0)[0], 2,
-                                                            replace=replace)
-            offspring[to_entry_idx] += 1
-            offspring[from_entry_idx] -= 1
-        return offspring
+        # add noise in 1 random coordinate for each offspring
+        noise = np.random.normal(scale=GeneticAlgorithmAgent.NOISE_FACTOR, size=len(offspring))
+        coords = np.random.randint(low=0, high=offspring.shape[1], size=offspring.shape[0])
+        noisy_offspring = offspring.copy()
+        noisy_offspring[:, coords] += np.clip(noisy_offspring[:, coords] + noise, a_min=0, a_max=np.inf)
+        noisy_offspring = noisy_offspring / noisy_offspring.sum(axis=-1)[:, None]
+        return noisy_offspring
+    def discretize(self, action_data: np.ndarray) -> np.ndarray:
+        data = action_data.copy()
+        assert np.isclose(np.sum(data), 1)
+        data *= self.agent_info.scooters_num
+        data_fraction, data_int = np.modf(data)
+
+        round_up_amount = np.sum(data_fraction)
+        # assert np.isclose(round_up_amount, round(round_up_amount)), f"{data}"
+        round_up_amount = round(round_up_amount)
+
+        data_fraction_flat, data_int_flat = data_fraction.flatten(), data_int.flatten()
+        data_fraction_index = np.argsort(-data_fraction_flat)
+        data_int_flat[data_fraction_index[:round_up_amount]] += 1
+        data_discrete = data_int_flat.reshape(data.shape)
+        assert np.sum(data_discrete) == self.agent_info.scooters_num
+        data_discrete = data_discrete.astype(int)
+        return data_discrete
+
