640e50147e572e0e7826572ea09449e8b1209ef4
roikohno <roikohno@gmail.com>
2022-08-23 23:23:16 +0300

    vis2 graph logs

--- programio/viz2.py
+++ programio/viz2.py
@@ -13,8 +13,8 @@ def visualize_log(log, interval=1000):
     sc = ax.scatter(x, y)
     plt.xlim(0,300)
     plt.ylim(0,300)
-    plt.xlabel("Incomes")
-    plt.ylabel("Expenses")
+    plt.xlabel("income")
+    plt.ylabel("expenses")
 
     def animate(i):
         frame = log[i + 1]
@@ -29,40 +29,75 @@ def visualize_log(log, interval=1000):
                     frames=len(log), interval=interval)
     return ani
 
+def viz_log(input_path, output_path):
+    with open(f'../runs/{input_path}', 'rb') as f:
+        log = pkl.load(f)
+        ani = visualize_log_single(log, interval=750)
+        ani.save(f'../visualizations/{output_path}')
+
+def viz_map(input_path, output_path):
+    with open(f'../runs/{input_path}', 'rb') as f:
+        log = pkl.load(f)
+        ani = map_visualization(log, interval=750)
+        ani.save(f'{output_path}')
+
+def visualize_log_single(log, interval=1000):
+
+    fig, ax = plt.subplots()
+    x, y = [], []
+    sc = ax.scatter(x, y)
+    plt.xlim(0,300)
+    plt.ylim(0,300)
+    plt.xlabel("income")
+    plt.ylabel("expenses")
+    def animate(i):
+        frame = log['total_incomes_expenses'][i]
+        x.clear()
+        y.clear()
+        x.append(frame['income'])
+        y.append(frame['expenses'])
+        sc.set_offsets(np.c_[x, y])
+
+    ani = matplotlib.animation.FuncAnimation(fig, animate,
+                    frames=len(log['total_incomes_expenses']), interval=interval)
+    return ani
+
+
 def viz_genetic_log():
-    with open('../genetic.pkl', 'rb') as f:
+    with open('../runs/genetic.pkl', 'rb') as f:
         log = pkl.load(f)
         ani = visualize_log(log, interval=750)
         ani.save('vid.mp4')
 
 Y_LIM = [32.076, 32.092]
 X_LIM = [34.77, 34.795]
-def map_visualization(nest_locations,
-                      log):
+def map_visualization(log, interval=100):
+    nest_locations = log['nest_locations']
     fig, ax = plt.subplots()
     map_img = mpimage.imread('tlv.png')
     plot_map(ax, map_img, X_LIM, Y_LIM)
-    ax.scatter([nest[1] for nest in nest_locations],
-               [nest[0] for nest in nest_locations], marker="X", s=50, c='red')
+    ax.scatter([nest.y for nest in nest_locations],
+               [nest.x for nest in nest_locations], marker="X", s=50, c='red')
     ax.axis('off')
     numbers_on_map = [ax.annotate('',
-                                  xy=(nest_locations[i][1] + 0.0005,
-                                      nest_locations[i][0] + 0.0005),
+                                  xy=(nest_locations[i].y + 0.0005,
+                                      nest_locations[i].x + 0.0005),
                                   size=14)
                       for i in range(len(nest_locations))]
     cur_rides = list()
     cur_reward = ax.text(0.03,.90,'Earnings:   ',
-                         transform=ax.transAxes,backgroundcolor='0.75')
+                         transform=ax.transAxes, backgroundcolor='0.75')
     def animate(i):
-        frame = log[i]
+        reward = log['last_game_money'][i]
+        num_scooters = [x.scooters_num for x in log['last_game_spread'][i]]
+        rides = log['last_game_rides'][i]
 
+        # update reward
+        cur_reward.set_text(f'Earnings: {round(reward, 2)}')
         # update num scooters
-        num_scooters = frame['scooter_spread']
-        rides = frame['rides']
-        reward = frame['reward']
-        cur_reward.set_text(f'Earnings: {reward}')
-        for i, a in enumerate(numbers_on_map):
-            a.set_text(num_scooters[i])
+        for j, a in enumerate(numbers_on_map):
+            a.set_text(num_scooters[j])
+
         # delete old rides
         for ride in cur_rides:
             ride.remove()
@@ -74,7 +109,7 @@ def map_visualization(nest_locations,
                                       dy, dx, color='darkgray',
                                       width=2e-5, head_width=2e-4))
     ani = matplotlib.animation.FuncAnimation(fig, animate,
-                    frames=len(log), interval=1000)
+                    frames=len(log['last_game_money']), interval=interval)
     return ani
 
 def plot_map(ax, img, x_lim, y_lim):
@@ -82,12 +117,11 @@ def plot_map(ax, img, x_lim, y_lim):
     ax.set_ylim(*y_lim)
     ax.imshow(img, extent=X_LIM + Y_LIM)
 
-
-
 def plot_frame():
     plt.show()
 
-if __name__ == '__main__':
+
+def demo_vis():
     log = {
         0: {
             'rides': [Ride(orig=Point(32.0853, 34.7818),
@@ -109,3 +143,7 @@ if __name__ == '__main__':
     ani = map_visualization([(32.0853, 34.7818),
                        (32.0883, 34.7848)], log)
     plt.show()
+
+if __name__ == '__main__':
+    # viz_log('dynamic_RL_cyclic_random.pkl', 'dynamic_RL_cyclic_random.mp4')
+    viz_map('human_cyclic_random.pkl', 'human_cyclic_random.mp4')
